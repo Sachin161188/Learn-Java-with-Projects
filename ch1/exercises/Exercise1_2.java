92e85eab2342018599a6df75cce91ab104233cff
Sachin <168698848+Sachin161188@users.noreply.github.com>
2024-05-10 14:03:29 +0530

    name Update Exercise1_2.java

--- ch1/exercises/Exercise1_2.java
+++ ch1/exercises/Exercise1_2.java
@@ -2,8 +2,8 @@ package ch1.exercises;
 
 public class Exercise1_2 {
     public static void main(String[] args) {
-        String name = "Maaike";
-        String position = "Park Manager";
-        System.out.println("My name is " + name + " and I want to be a " + position + " in Mesozoic Eden.");
+        String name = "Sachin";
+        String position = "QA Tester";
+        System.out.println("My name is " + name + " and I want to be a " + position + " in Wipro.");
     }
 }
